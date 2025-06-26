1496606bcddb67b0874d5ea5fb0d9a1201040f21
Uriel Aguilera <uriel.aguilera@tecmm.edu.mx>
2025-06-26 14:22:58 -0600

    autenticación

--- router/noticias.js
+++ router/noticias.js
@@ -1,12 +1,13 @@
 const { Router } = require("express");
 const {registrar_noticia, obtener_noticias, obtener_noticias_one, actualizar_noticia, eliminar_noticia} = require('../controllers/noticias');
+const {authenticateClient} = require("../middlewares/authenticate")
 
 const routerNoticias = Router();
 
-routerNoticias.post('/', registrar_noticia);
+routerNoticias.post('/', authenticateClient, registrar_noticia);
 routerNoticias.get('/', obtener_noticias);
 routerNoticias.get('/:idNew', obtener_noticias_one);
-routerNoticias.put('/:idNew', actualizar_noticia);
-routerNoticias.delete('/:idNew', eliminar_noticia);
+routerNoticias.put('/:idNew', authenticateClient, actualizar_noticia);
+routerNoticias.delete('/:idNew', authenticateClient, eliminar_noticia);
 
 module.exports = routerNoticias;
