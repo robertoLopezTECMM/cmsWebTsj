b2811b0e1d8ebf5aa2b7ad6113851dc8ee7067d1
Uriel Aguilera <uriel.aguilera@tecmm.edu.mx>
2025-08-25 14:23:57 -0600

    mensaje bienvenida

--- controllers/video.js
+++ controllers/video.js
@@ -38,4 +38,39 @@ const video = async (req, res) => {
     //res.status(200).json({ok: true})
 };
 
-module.exports = {video}
+
+const video2 = async (req, res) => {
+    const videoPath = path.join(__dirname, '..', 'public', 'cmsWebFiles', 'mensajeBienvenida.mp4');
+    const stat = fs.statSync(videoPath);
+    const fileSize = stat.size;
+    const range = req.headers.range;
+
+    if (range) {
+        // Maneja solicitudes de streaming (parciales)
+        const parts = range.replace(/bytes=/, "").split("-");
+        const start = parseInt(parts[0], 10);
+        const end = parts[1] ? parseInt(parts[1], 10) : fileSize - 1;
+        const chunksize = (end - start) + 1;
+        const file = fs.createReadStream(videoPath, { start, end });
+        const head = {
+          'Content-Range': `bytes ${start}-${end}/${fileSize}`,
+          'Accept-Ranges': 'bytes',
+          'Content-Length': chunksize,
+          'Content-Type': 'video/mp4',
+        };
+
+        res.writeHead(206, head);
+        file.pipe(res);
+    } else {
+        // Maneja la solicitud completa del video
+        const head = {
+          'Content-Length': fileSize,
+          'Content-Type': 'video/mp4',
+        };
+        res.writeHead(200, head);
+        fs.createReadStream(videoPath).pipe(res);
+    }
+};
+
+
+module.exports = {video, video2}
